170a700a6bd901268f465a29a88a559897a0ce6c
Marcin00Czarnecki <marcin00czarnecki@gmail.com>
2021-05-06 20:27:05 +0200

    Excercise 2.1 Vowels

--- Chapter2/Vowels.py
+++ Chapter2/Vowels.py
@@ -2,7 +2,12 @@ vowels = ['a', 'e', 'i', 'o', 'u']
 
 world = "Miliard"
 
+found = []
+
 for letter in world:
     if letter in vowels:
-        print(letter)
+        if letter not in found:
+            found.append(letter)
+for vowel in found:
+    print(vowel)
 
