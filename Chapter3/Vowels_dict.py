1f16f556c1fa4ea3013fbbd79eee9bbc2bb41e6a
Marcin00Czarnecki <marcin00czarnecki@gmail.com>
2021-05-11 18:56:55 +0200

    Excercise 3.1 Vowels_dict ver2

--- Chapter3/Vowels_dict.py
+++ Chapter3/Vowels_dict.py
@@ -3,15 +3,12 @@ vowels = ['a', 'e', 'i', 'o', 'u']
 world = input("Podaj słowo, w którym należy wyszukać samogłoski: ")
 
 found = {}
-found['a'] = 0
-found['e'] = 0
-found['i'] = 0
-found['o'] = 0
-found['u'] = 0
 
 for letter in world:
     if letter in vowels:
+        found.setdefault(letter, 0) 
         found[letter] += 1
-for k, v in found.items():
+for k, v in sorted(found.items()):
     print(k,'znaleziono', v, 'raz(y).')
 
+print(found)
